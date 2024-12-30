95f3b9b86b0724513f27d9a8da0a85da6514170d
malla-lokesh <lokeshmvs21@gmail.com>
2024-12-30 17:23:58 +0530

    chore: changed the href for navLinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -115,7 +115,7 @@ const Navbar = ({
               {navLinks.map(({ label, children, href = '', newTab }) => (
                 <li key={label}>
                   <Link
-                    href={`/category/${href}`}
+                    href={`/products?category=${href.substring(1, href.length)}`}
                     target={newTab ? '_blank' : '_self'}
                     className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none'>
                     {label}
