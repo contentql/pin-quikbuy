d25ba241e188f6930645382c11f4aba00671378e
malla-lokesh <lokeshmvs21@gmail.com>
2025-01-06 13:39:53 +0530

    chore: products data

--- src/seed/categories/data.ts
+++ src/seed/categories/data.ts
@@ -1,29 +1,43 @@
-export const categoriesData = [
-  {
-    name: 'Apparel',
-    imageURL: [
-      {
-        url: process.cwd() + '/public/images/seed/categories/apparel.webp',
-        alt: 'apparel image',
-      },
-    ],
-  },
-  {
-    name: 'Accessories',
-    imageURL: [
-      {
-        url: process.cwd() + '/public/images/seed/categories/accessories.webp',
-        alt: 'accessories image',
-      },
-    ],
-  },
-  {
-    name: 'Digital',
-    imageURL: [
-      {
-        url: process.cwd() + '/public/images/seed/categories/digital.webp',
-        alt: 'digital image',
-      },
-    ],
-  },
-]
+import { Page } from '@payload-types'
+
+export type CategoriesDataType = Omit<Page, 'id' | 'createdAt' | 'updatedAt'>
+
+export type CategoriesImageType = {
+  alt: string
+  filePath: string
+}
+
+export const categoriesData: CategoriesDataType = {
+  title: 'Categories',
+  layout: [{}],
+}
+
+// export const categoriesData = [
+//   {
+//     name: 'Apparel',
+//     imageURL: [
+//       {
+//         url: process.cwd() + '/public/images/seed/categories/apparel.webp',
+//         alt: 'apparel image',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'Accessories',
+//     imageURL: [
+//       {
+//         url: process.cwd() + '/public/images/seed/categories/accessories.webp',
+//         alt: 'accessories image',
+//       },
+//     ],
+//   },
+//   {
+//     name: 'Digital',
+//     imageURL: [
+//       {
+//         url: process.cwd() + '/public/images/seed/categories/digital.webp',
+//         alt: 'digital image',
+//       },
+//     ],
+//   },
+// ]
---
+++ src/seed/products/data.ts
@@ -0,0 +1,52 @@
+import path from 'path'
+import { RequiredDataFromCollectionSlug } from 'payload'
+
+export type ProductDataType = RequiredDataFromCollectionSlug<'products'>
+export type ProductImageType = {
+  alt: string
+  filePath: string
+}
+
+export const projectsData: ProductDataType[] = [
+  {
+    name: 'Horizon Gaze Sunglasses',
+    slug: 'horizon-gaze-sunglasses',
+    description:
+      'Protect your eyes in style with these chic sunglasses. Featuring polarized lenses, they offer superior UV protection. The lightweight frame provides comfort and a timeless look.tem',
+    brand: 'temp',
+    stock: 10,
+    price: 20,
+
+    discount: {
+      percentage: null,
+      startDate: null,
+      endDate: null,
+    },
+    finalPrice: 20,
+
+    category: ,
+
+    tags: [],
+
+    attributes: [],
+
+    images: [],
+    isFeatured: false,
+    isNewArrival: false,
+    isSpecialOffer: false,
+    isShippable: true,
+
+    additionalInformationSections: [],
+    _status: 'published',
+  },
+]
+
+export const projectsImagesData: ProductImageType[] = [
+  {
+    alt: 'Project 1',
+    filePath: path.join(
+      process.cwd(),
+      '/public/images/seed/products/sun-glasses.webp',
+    ),
+  },
+]
