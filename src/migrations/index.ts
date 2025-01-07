105dcecaa5cc2a7517ec7a60b8bf667ffb43ce9e
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-01-07 22:53:21 +0530

    chore: generated correct migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,5 @@
 import * as migration_20250101_111849_init_migration from './20250101_111849_init_migration';
-import * as migration_20250107_170150 from './20250107_170150';
+import * as migration_20250107_172212 from './20250107_172212';
 
 export const migrations = [
   {
@@ -8,8 +8,8 @@ export const migrations = [
     name: '20250101_111849_init_migration',
   },
   {
-    up: migration_20250107_170150.up,
-    down: migration_20250107_170150.down,
-    name: '20250107_170150'
+    up: migration_20250107_172212.up,
+    down: migration_20250107_172212.down,
+    name: '20250107_172212'
   },
 ];
