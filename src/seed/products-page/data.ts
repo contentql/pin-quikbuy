aadef961b20c95a3ae38769d0c82b39962442e59
malla-lokesh <lokeshmvs21@gmail.com>
2025-01-07 10:28:07 +0530

    chore: added products-page seed data

---
+++ src/seed/products-page/data.ts
@@ -0,0 +1,39 @@
+export const productsPage = {
+  //   id: 2,
+  title: 'Products',
+
+  layout: [
+    {
+      id: '677531f9df41dccc1e7895f1',
+      title: 'All Products',
+      collectionSlug: 'products',
+      blockName: null,
+      blockType: 'List',
+    },
+  ],
+
+  meta: {
+    title: null,
+    description: null,
+    image: null,
+  },
+  isHome: false,
+  isDynamic: false,
+  slugMode: 'generate',
+  slug: 'products',
+  pathMode: 'generate',
+  path: '/products',
+  parent: null,
+
+  breadcrumbs: [
+    {
+      id: '67753226c405f90001543f86',
+      doc: 2,
+      url: '/products',
+      label: 'Products',
+    },
+  ],
+  //   updatedAt: '2025-01-01T12:16:38.518Z',
+  //   createdAt: '2025-01-01T12:16:37.102Z',
+  _status: 'published',
+}
