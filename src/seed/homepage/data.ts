795003b47acaf4033661e6b9021448a64ff15eab
malla-lokesh <lokeshmvs21@gmail.com>
2025-01-07 10:21:35 +0530

    chore: added homepage seed data

---
+++ src/seed/homepage/data.ts
@@ -0,0 +1,241 @@
+export const homepageData = {
+  //   id: 1,
+  title: 'Home Page',
+
+  layout: [
+    {
+      id: '6775311177b2cd00ce08142c',
+      title: 'Discover our Curated Collection',
+      description:
+        'Explore our carefully selected products for your home and lifestyle.',
+      blockName: null,
+      blockType: 'Home',
+    },
+
+    {
+      id: '6775313577b2cd00ce08142d',
+      blockName: null,
+
+      featuredProducts: [
+        {
+          id: '6775313977b2cd00ce08142e',
+
+          products: [
+            {
+              id: 1,
+              name: 'Sunbeam Tote',
+              slug: 'sunbeam-tote',
+              description:
+                "Brighten your day with this cheerful yellow bag. Its vibrant color and ample storage space make it an excellent choice for shopping or day trips. Lightweight and durable, it's designed for comfort and convenience. Test",
+              brand: 'temp',
+              stock: 5,
+              price: 25,
+
+              discount: {
+                percentage: 5,
+                startDate: '2025-01-02T11:30:00.000Z',
+                endDate: '2025-01-04T11:30:00.000Z',
+              },
+              finalPrice: 23.75,
+              category: 1,
+
+              tags: [],
+
+              attributes: [],
+
+              images: [3, 4, 5, 6],
+              isFeatured: false,
+              isNewArrival: false,
+              isSpecialOffer: false,
+              isShippable: true,
+
+              additionalInformationSections: [],
+              snipcartId: null,
+              updatedAt: '2025-01-05T15:23:29.989Z',
+              createdAt: '2025-01-01T12:06:09.590Z',
+              _status: 'published',
+            },
+          ],
+        },
+
+        {
+          id: '677537ae4a83b61299800007',
+
+          products: [
+            {
+              id: 2,
+              name: 'ShadowStride Shoes',
+              slug: 'shadowstride-shoes',
+              description:
+                'These classic black shoes are a wardrobe essential. Designed for both comfort and versatility, they pair well with any attire. The cushioned sole ensures all-day comfort.',
+              brand: 'temp2',
+              stock: 10,
+              price: 20,
+
+              discount: {
+                percentage: null,
+                startDate: null,
+                endDate: null,
+              },
+              finalPrice: 20,
+              category: 1,
+
+              tags: [],
+
+              attributes: [],
+
+              images: [9],
+              isFeatured: false,
+              isNewArrival: false,
+              isSpecialOffer: false,
+              isShippable: true,
+
+              additionalInformationSections: [],
+              snipcartId: null,
+              updatedAt: '2025-01-01T12:15:58.674Z',
+              createdAt: '2025-01-01T12:15:58.674Z',
+              _status: 'published',
+            },
+          ],
+        },
+
+        {
+          id: '67753bbd74329152d72c18f9',
+
+          products: [
+            {
+              id: 3,
+              name: 'Horizon Gaze Sunglasses',
+              slug: 'horizon-gaze-sunglasses',
+              description:
+                'Protect your eyes in style with these chic sunglasses. Featuring polarized lenses, they offer superior UV protection. The lightweight frame provides comfort and a timeless look.tem',
+              brand: 'temp',
+              stock: 10,
+              price: 20,
+
+              discount: {
+                percentage: null,
+                startDate: null,
+                endDate: null,
+              },
+              finalPrice: 20,
+              category: 2,
+
+              tags: [],
+
+              attributes: [],
+
+              images: [17],
+              isFeatured: false,
+              isNewArrival: false,
+              isSpecialOffer: false,
+              isShippable: true,
+
+              additionalInformationSections: [],
+              snipcartId: null,
+              updatedAt: '2025-01-01T12:57:07.790Z',
+              createdAt: '2025-01-01T12:57:07.790Z',
+              _status: 'published',
+            },
+          ],
+        },
+      ],
+      blockType: 'FeaturedProducts',
+    },
+
+    {
+      id: '67753180df41dccc1e7895ef',
+      blockName: null,
+
+      categories: [
+        {
+          id: '67753184df41dccc1e7895f0',
+
+          category: {
+            id: 1,
+            name: 'Apparel',
+            slug: 'apparel',
+            description: null,
+            parentCategory: null,
+
+            subCategories: [],
+            isFeatured: false,
+            image: 2,
+
+            products: {
+              docs: [2, 1],
+              hasNextPage: false,
+            },
+            productCount: 2,
+
+            meta: {
+              title: null,
+              description: null,
+              image: null,
+            },
+            updatedAt: '2025-01-05T15:23:30.576Z',
+            createdAt: '2025-01-01T12:02:43.483Z',
+            _status: 'published',
+          },
+        },
+
+        {
+          id: '67753bdf74329152d72c18fa',
+
+          category: {
+            id: 2,
+            name: 'Accessories',
+            slug: 'accessories',
+            description: null,
+            parentCategory: null,
+
+            subCategories: [],
+            isFeatured: false,
+            image: 16,
+
+            products: {
+              docs: [3],
+              hasNextPage: false,
+            },
+            productCount: 1,
+
+            meta: {
+              title: null,
+              description: null,
+              image: null,
+            },
+            updatedAt: '2025-01-01T12:57:08.534Z',
+            createdAt: '2025-01-01T12:55:27.071Z',
+            _status: 'published',
+          },
+        },
+      ],
+      blockType: 'Categories',
+    },
+  ],
+
+  meta: {
+    title: null,
+    description: null,
+    image: null,
+  },
+  isHome: true,
+  isDynamic: false,
+  slugMode: 'generate',
+  slug: 'home-page',
+  pathMode: 'generate',
+  path: '/',
+  parent: null,
+
+  breadcrumbs: [
+    {
+      id: '67753be5ead83b00012c7a90',
+      doc: 1,
+      url: '/',
+      label: 'Home Page',
+    },
+  ],
+  //   updatedAt: '2025-01-01T12:58:13.995Z',
+  //   createdAt: '2025-01-01T12:12:47.908Z',
+  _status: 'published',
+}
