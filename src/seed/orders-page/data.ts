9c36dd0b8609988300832c683fd6e1c33a3b9038
malla-lokesh <lokeshmvs21@gmail.com>
2025-01-07 10:50:07 +0530

    chore: added orderspage seed data and renamed homepage seed file

---
+++ src/seed/orders-page/data.ts
@@ -0,0 +1,39 @@
+export const ordersPageData = {
+  //   id: 7,
+  title: 'orders',
+
+  layout: [
+    {
+      id: '677535664a83b61299800006',
+      title: null,
+      collectionSlug: 'orders',
+      blockName: null,
+      blockType: 'List',
+    },
+  ],
+
+  meta: {
+    title: null,
+    description: null,
+    image: null,
+  },
+  isHome: false,
+  isDynamic: false,
+  slugMode: 'generate',
+  slug: 'orders',
+  pathMode: 'generate',
+  path: '/orders',
+  parent: null,
+
+  breadcrumbs: [
+    {
+      id: '6775356d281b780001dd8243',
+      doc: 7,
+      url: '/orders',
+      label: 'orders',
+    },
+  ],
+  //   updatedAt: '2025-01-01T12:30:37.130Z',
+  //   createdAt: '2025-01-01T12:30:35.805Z',
+  _status: 'published',
+}
