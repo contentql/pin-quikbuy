7ad5699a6be3a9556d56d54d52831a9d0b96b9c8
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-01-07 09:48:05 +0530

    chore: categories seed

---
+++ src/seed/categories/index.ts
@@ -0,0 +1,6 @@
+// Exporting types and constants from data.ts
+export { categoriesData, categoriesImagesData } from './data'
+export type { CategoriesDataType, CategoriesImagesType } from './data'
+
+// Exporting types and functions from seed.ts
+export { default as seedCategories } from './seed'
--- src/seed/categories/seed.ts
+++ src/seed/categories/seed.ts
@@ -1,11 +1,71 @@
+import configPromise from '@payload-config'
+import { Category } from '@payload-types'
 import { Ora } from 'ora'
+import { getPayload } from 'payload'
 
-export const seedCategories = async ({ spinner }: { spinner: Ora }) => {
-  spinner.start(`Started creating categories...`)
+import { CategoryDataType, categoriesData, categoriesImagesData } from './data'
+
+const payload = await getPayload({ config: configPromise })
+
+const seed = async (spinner: Ora): Promise<(string | Category)[]> => {
   try {
-    spinner.succeed(`Successfully created categories...`)
+    spinner.start(`Started created categories...`)
+
+    const categoryImagesResult = await Promise.allSettled(
+      categoriesImagesData.map(image =>
+        payload.create({
+          collection: 'media',
+          data: {
+            alt: image.alt,
+          },
+          filePath: image.filePath,
+        }),
+      ),
+    )
+    const formattedCategoryImagesResult = categoryImagesResult
+      .map(result =>
+        result.status === 'fulfilled'
+          ? result.value
+          : `Failed to seed: ${result.reason}`,
+      )
+      .filter(result => typeof result !== 'string')
+
+    let categoryImageIndex = 0
+
+    const formattedCategoriesData: CategoryDataType[] = categoriesData.map(
+      category => {
+        const formattedCategory: CategoryDataType = {
+          ...category,
+          image: formattedCategoryImagesResult[categoryImageIndex].id,
+        }
+        categoryImageIndex++
+
+        return formattedCategory
+      },
+    )
+
+    const results = await Promise.allSettled(
+      formattedCategoriesData.map(categoryData =>
+        payload.create({
+          collection: 'categories',
+          data: categoryData,
+        }),
+      ),
+    )
+    const formattedResults = results.map(result =>
+      result.status === 'fulfilled'
+        ? result.value
+        : `Failed to seed: ${result.reason}`,
+    )
+
+    spinner.start(`Successfully created categories.`)
+
+    return formattedResults
   } catch (error) {
-    spinner.fail(`Failed creating categories...`)
+    spinner.succeed(`Failed to create categories`)
+
     throw error
   }
 }
+
+export default seed
