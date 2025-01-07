9cefbab17682d10a2047fe237f26b57c0d9bb1af
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2025-01-07 13:46:39 +0530

    chore: site settings seed

--- src/seed/site-settings/data.ts
+++ src/seed/site-settings/data.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { DataFromGlobalSlug } from 'payload'
 
-export type siteSettingsDataType = Omit<
+export type SiteSettingsDataType = Omit<
   DataFromGlobalSlug<'site-settings'>,
   'id'
 >
@@ -11,102 +11,14 @@ export type ImageType = {
   filePath: string
 }
 
-export const siteSettingsData: siteSettingsDataType = {
-  // id: 1,
-
+export const siteSettingsData: SiteSettingsDataType = {
   general: {
     title: 'Quikbuy',
     description: 'Quick & easy purchasing focus.',
 
-    faviconUrl: {
-      id: 14,
-      alt: 'quik-buy-svg',
-      updatedAt: '2025-01-01T12:26:04.696Z',
-      createdAt: '2025-01-01T12:26:04.696Z',
-      url: '/api/media/file/quik-buy-1.svg',
-      thumbnailURL: null,
-      filename: 'quik-buy-1.svg',
-      mimeType: 'image/svg+xml',
-      filesize: 793,
-      width: 50,
-      height: 50,
-      focalX: null,
-      focalY: null,
+    faviconUrl: 0,
 
-      sizes: {
-        thumbnail: {
-          url: null,
-          width: null,
-          height: null,
-          mimeType: null,
-          filesize: null,
-          filename: null,
-        },
-
-        blogImageSize2: {
-          url: null,
-          width: null,
-          height: null,
-          mimeType: null,
-          filesize: null,
-          filename: null,
-        },
-
-        blogImageSize3: {
-          url: null,
-          width: null,
-          height: null,
-          mimeType: null,
-          filesize: null,
-          filename: null,
-        },
-      },
-    },
-
-    ogImageUrl: {
-      id: 15,
-      alt: 'open-graph-image',
-      updatedAt: '2025-01-01T12:26:43.147Z',
-      createdAt: '2025-01-01T12:26:43.147Z',
-      url: '/api/media/file/open-graph-image.webp',
-      thumbnailURL: null,
-      filename: 'open-graph-image.webp',
-      mimeType: 'image/webp',
-      filesize: 87282,
-      width: 3840,
-      height: 2192,
-      focalX: 50,
-      focalY: 50,
-
-      sizes: {
-        thumbnail: {
-          url: '/api/media/file/open-graph-image-400x300.webp',
-          width: 400,
-          height: 300,
-          mimeType: 'image/webp',
-          filesize: 6332,
-          filename: 'open-graph-image-400x300.webp',
-        },
-
-        blogImageSize2: {
-          url: '/api/media/file/open-graph-image-986x563.webp',
-          width: 986,
-          height: 563,
-          mimeType: 'image/webp',
-          filesize: 15304,
-          filename: 'open-graph-image-986x563.webp',
-        },
-
-        blogImageSize3: {
-          url: '/api/media/file/open-graph-image-1470x839.webp',
-          width: 1470,
-          height: 839,
-          mimeType: 'image/webp',
-          filesize: 25432,
-          filename: 'open-graph-image-1470x839.webp',
-        },
-      },
-    },
+    ogImageUrl: 1,
 
     keywords: ['e-commerce'],
     currency: 'usd',
@@ -114,102 +26,26 @@ export const siteSettingsData: siteSettingsDataType = {
 
   navbar: {
     logo: {
-      imageUrl: {
-        id: 13,
-        alt: 'quik-buy',
-        updatedAt: '2025-01-01T12:23:37.036Z',
-        createdAt: '2025-01-01T12:23:37.036Z',
-        url: '/api/media/file/quik-buy.png',
-        thumbnailURL: null,
-        filename: 'quik-buy.png',
-        mimeType: 'image/png',
-        filesize: 499,
-        width: 50,
-        height: 50,
-        focalX: 50,
-        focalY: 50,
-
-        sizes: {
-          thumbnail: {
-            url: null,
-            width: null,
-            height: null,
-            mimeType: null,
-            filesize: null,
-            filename: null,
-          },
-
-          blogImageSize2: {
-            url: '/api/media/file/quik-buy-986x986.png',
-            width: 986,
-            height: 986,
-            mimeType: 'image/png',
-            filesize: 109113,
-            filename: 'quik-buy-986x986.png',
-          },
-
-          blogImageSize3: {
-            url: '/api/media/file/quik-buy-1470x1470.png',
-            width: 1470,
-            height: 1470,
-            mimeType: 'image/png',
-            filesize: 183420,
-            filename: 'quik-buy-1470x1470.png',
-          },
-        },
-      },
+      imageUrl: 0,
       height: null,
       width: null,
     },
 
     menuLinks: [
       {
-        id: '677534985b30ca73ea7dff32',
         group: false,
 
         menuLink: {
-          type: 'reference',
+          type: 'custom',
           newTab: null,
           icon: null,
           label: 'Apparel',
 
+          url: null,
           page: {
             relationTo: 'pages',
-
-            value: {
-              id: 4,
-              title: 'Apparel',
-
-              meta: {
-                title: null,
-                description: null,
-                image: null,
-              },
-              isHome: false,
-              isDynamic: false,
-              slugMode: 'generate',
-              slug: 'apparel',
-              pathMode: 'generate',
-              path: '/apparel',
-              parent: null,
-
-              breadcrumbs: [
-                {
-                  id: '67753416c405f90001543f92',
-                  doc: 4,
-                  url: '/apparel',
-                  label: 'Apparel',
-                },
-              ],
-              updatedAt: '2025-01-01T12:24:54.665Z',
-              createdAt: '2025-01-01T12:24:53.492Z',
-              _status: 'published',
-
-              layout: [],
-            },
+            value: 0,
           },
-          url: null,
-          id: '677cd0b1e38d28000151ed5c',
         },
 
         menuLinkGroup: {
@@ -220,7 +56,6 @@ export const siteSettingsData: siteSettingsDataType = {
       },
 
       {
-        id: '677534a45b30ca73ea7dff33',
         group: false,
 
         menuLink: {
@@ -231,41 +66,9 @@ export const siteSettingsData: siteSettingsDataType = {
 
           page: {
             relationTo: 'pages',
-
-            value: {
-              id: 5,
-              title: 'Accessories',
-
-              meta: {
-                title: null,
-                description: null,
-                image: null,
-              },
-              isHome: false,
-              isDynamic: false,
-              slugMode: 'generate',
-              slug: 'accessories',
-              pathMode: 'generate',
-              path: '/accessories',
-              parent: null,
-
-              breadcrumbs: [
-                {
-                  id: '67753423c405f90001543f94',
-                  doc: 5,
-                  url: '/accessories',
-                  label: 'Accessories',
-                },
-              ],
-              updatedAt: '2025-01-01T12:25:07.096Z',
-              createdAt: '2025-01-01T12:25:05.956Z',
-              _status: 'published',
-
-              layout: [],
-            },
+            value: 0,
           },
           url: null,
-          id: '677cd0b1e38d28000151ed5e',
         },
 
         menuLinkGroup: {
@@ -276,7 +79,6 @@ export const siteSettingsData: siteSettingsDataType = {
       },
 
       {
-        id: '677534ae5b30ca73ea7dff34',
         group: false,
 
         menuLink: {
@@ -287,41 +89,9 @@ export const siteSettingsData: siteSettingsDataType = {
 
           page: {
             relationTo: 'pages',
-
-            value: {
-              id: 6,
-              title: 'Digital',
-
-              meta: {
-                title: null,
-                description: null,
-                image: null,
-              },
-              isHome: false,
-              isDynamic: false,
-              slugMode: 'generate',
-              slug: 'digital',
-              pathMode: 'generate',
-              path: '/digital',
-              parent: null,
-
-              breadcrumbs: [
-                {
-                  id: '6775342cc405f90001543f96',
-                  doc: 6,
-                  url: '/digital',
-                  label: 'Digital',
-                },
-              ],
-              updatedAt: '2025-01-01T12:25:16.070Z',
-              createdAt: '2025-01-01T12:25:14.912Z',
-              _status: 'published',
-
-              layout: [],
-            },
+            value: 0,
           },
           url: null,
-          id: '677cd0b1e38d28000151ed60',
         },
 
         menuLinkGroup: {
@@ -335,50 +105,7 @@ export const siteSettingsData: siteSettingsDataType = {
 
   footer: {
     logo: {
-      imageUrl: {
-        id: 13,
-        alt: 'quik-buy',
-        updatedAt: '2025-01-01T12:23:37.036Z',
-        createdAt: '2025-01-01T12:23:37.036Z',
-        url: '/api/media/file/quik-buy.png',
-        thumbnailURL: null,
-        filename: 'quik-buy.png',
-        mimeType: 'image/png',
-        filesize: 499,
-        width: 50,
-        height: 50,
-        focalX: 50,
-        focalY: 50,
-
-        sizes: {
-          thumbnail: {
-            url: null,
-            width: null,
-            height: null,
-            mimeType: null,
-            filesize: null,
-            filename: null,
-          },
-
-          blogImageSize2: {
-            url: '/api/media/file/quik-buy-986x986.png',
-            width: 986,
-            height: 986,
-            mimeType: 'image/png',
-            filesize: 109113,
-            filename: 'quik-buy-986x986.png',
-          },
-
-          blogImageSize3: {
-            url: '/api/media/file/quik-buy-1470x1470.png',
-            width: 1470,
-            height: 1470,
-            mimeType: 'image/png',
-            filesize: 183420,
-            filename: 'quik-buy-1470x1470.png',
-          },
-        },
-      },
+      imageUrl: 1,
       height: null,
       width: null,
       description: null,
@@ -394,51 +121,7 @@ export const siteSettingsData: siteSettingsDataType = {
     productLink: {
       relationTo: 'pages',
 
-      value: {
-        id: 3,
-        title: 'Product Details',
-
-        layout: [
-          {
-            id: '6775323a5b30ca73ea7dff30',
-            collectionSlug: 'products',
-            blockName: null,
-            blockType: 'Details',
-          },
-        ],
-
-        meta: {
-          title: null,
-          description: null,
-          image: null,
-        },
-        isHome: false,
-        isDynamic: true,
-        slugMode: 'generate',
-        slug: 'product-details',
-        pathMode: 'generate',
-        path: '/product/[product-details]',
-        parent: 2,
-
-        breadcrumbs: [
-          {
-            id: '67753270c405f90001543f8a',
-            doc: 2,
-            url: '/products',
-            label: 'Products',
-          },
-
-          {
-            id: '67753270c405f90001543f8b',
-            doc: 3,
-            url: '/product/[product-details]',
-            label: 'Product Details',
-          },
-        ],
-        updatedAt: '2025-01-01T12:17:52.710Z',
-        createdAt: '2025-01-01T12:17:51.070Z',
-        _status: 'published',
-      },
+      value: 1,
     },
   },
 
@@ -485,21 +168,15 @@ export const siteSettingsData: siteSettingsDataType = {
     },
     radius: 'none',
   },
-
-  productInformation: {
-    currency: null,
-  },
-  // updatedAt: '2025-01-07T06:11:18.572Z',
-  createdAt: null,
 }
 
 export const siteSettingsImages: ImageType[] = [
   {
-    alt: 'og-image',
-    filePath: path.join(process.cwd(), '/public/images/seed/og-image.png'),
-  },
-  {
     alt: 'favicon-url',
     filePath: path.join(process.cwd(), '/public/images/seed/quik-buy-logo.png'),
+  },
+  {
+    alt: 'og-image',
+    filePath: path.join(process.cwd(), '/public/images/seed/og-image.png'),
   },
 ]
