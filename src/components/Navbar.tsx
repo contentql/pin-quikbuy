c604b0208cbf1d2ac09c4616e3ce383a56995b7c
malla-lokesh <lokeshmvs21@gmail.com>
2025-01-08 11:50:18 +0530

    changed the redirection link for nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar = ({
               {navLinks.map(({ label, children, href = '', newTab }) => (
                 <li key={label}>
                   <Link
-                    href={`/products?category=${href.substring(1, href.length)}`}
+                    href={`/products?category=${label}`}
                     target={newTab ? '_blank' : '_self'}
                     className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none'>
                     {label}
