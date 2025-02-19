e60ce736df5763e4b1452cdb40b6b219536736b3
charanm927 <charanm927@gmail.com>
2025-02-19 21:59:57 +0530

    chore: update navBar links as categories and pages

--- src/app/(app)/(marketing)/layout.tsx
+++ src/app/(app)/(marketing)/layout.tsx
@@ -1,4 +1,6 @@
+import configPromise from '@payload-config'
 import { headers } from 'next/headers'
+import { getPayload } from 'payload'
 
 import Branding from '@/components/Branding'
 import Footer from '@/components/Footer'
@@ -13,10 +15,22 @@ const MarketingLayout = async ({ children }: { children: React.ReactNode }) => {
   const headersList = await headers()
   const user = await getCurrentUser(headersList)
 
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const { docs: categoriesData } = await payload.find({
+    collection: 'categories',
+  })
+
   return (
     <MetadataProvider metadata={metadata}>
       <div className='flex min-h-full flex-1 flex-col bg-white'>
-        <Navbar metadata={metadata} user={user} />
+        <Navbar
+          metadata={metadata}
+          user={user}
+          categoriesData={categoriesData}
+        />
         {/* <SecondNavbar /> */}
         <main className='mx-auto flex w-full max-w-7xl flex-1 flex-col px-4 pb-6 pt-2 sm:px-6 lg:px-8'>
           {children}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import type { SiteSetting, User } from '@payload-types'
-import { SearchIcon } from 'lucide-react'
+import type { Category, SiteSetting, User } from '@payload-types'
+import { ChevronDown, SearchIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Suspense } from 'react'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/common/Dropdown'
 import { CartSummaryNav } from '@/ui/nav/cart-summary-nav'
 import {
   SearchInput,
@@ -18,9 +24,11 @@ import ProfileDropdown from './ProfileDropdown'
 const Navbar = ({
   metadata,
   user,
+  categoriesData,
 }: {
   metadata: SiteSetting
   user: User | null
+  categoriesData: Category[]
 }) => {
   const { navbar } = metadata
   const { logo, menuLinks } = navbar
@@ -51,6 +59,10 @@ const Navbar = ({
 
   const { height, width } = logo
 
+  function capitalizeFirstLetter(val: string) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1)
+  }
+
   return (
     <header className='nav-border-reveal sticky top-0 z-20 bg-white/90 py-4 backdrop-blur-sm'>
       <div className='mx-auto flex max-w-7xl flex-row items-center gap-2 px-4 sm:px-6 lg:px-8'>
@@ -60,7 +72,7 @@ const Navbar = ({
         </div> */}
         {logoDetails.url && (
           <div>
-            <Link href='/' className='flex flex-1 gap-x-2'>
+            <Link href='/' className='flex items-center gap-x-2'>
               <div className='-mt-0.5 whitespace-nowrap text-xl font-bold'>
                 QuikBuy
               </div>
@@ -77,24 +89,62 @@ const Navbar = ({
         <div className='flex w-auto max-w-full flex-shrink overflow-auto max-sm:order-2 sm:mr-auto'>
           <div className='hidden sm:block'>
             <ul className='flex flex-row items-center justify-center gap-x-1'>
-              {navLinks.map(({ label, children, href = '', newTab }) => {
-                const category = label.toLowerCase()
-                return (
-                  <li key={label}>
-                    <Link
-                      href={`/products?category=${category}`}
-                      target={newTab ? '_blank' : '_self'}
-                      className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none'>
-                      {label}
-                    </Link>
-                  </li>
-                )
-              })}
+              {categoriesData &&
+                (categoriesData?.length > 3 ? (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className='group hidden h-9 w-max items-center justify-center gap-1 rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none lg:flex'>
+                      <span> Categories</span>
+                      <ChevronDown size={20} />
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent
+                      className='z-[60] max-w-56'
+                      align='center'>
+                      {categoriesData?.map((category, index) => (
+                        <Link
+                          key={index}
+                          href={`/products?category=${category?.name.toLowerCase()}`}>
+                          <DropdownMenuItem className='cursor-pointer'>
+                            {capitalizeFirstLetter(category?.name)}
+                          </DropdownMenuItem>
+                        </Link>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                ) : (
+                  categoriesData?.map((category, index) => (
+                    <li key={index}>
+                      <Link
+                        href={`/products?category=${category?.name.toLowerCase()}`}
+                        className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none'>
+                        {capitalizeFirstLetter(category?.name)}
+                      </Link>
+                    </li>
+                  ))
+                ))}
             </ul>
           </div>
         </div>
 
-        <div className='ml-auto mr-3 sm:ml-0'>
+        <div className='ml-auto mr-3 flex items-center sm:ml-0'>
+          <div className='flex w-auto max-w-full flex-shrink max-sm:order-2 sm:mr-auto'>
+            <div className='hidden sm:block'>
+              <ul className='flex flex-row items-center justify-center gap-x-1'>
+                {navLinks.map(({ label, children, href = '', newTab }) => {
+                  return (
+                    <li key={label}>
+                      <Link
+                        href={href}
+                        target={newTab ? '_blank' : '_self'}
+                        className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none'>
+                        {label}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
           <label className='flex w-full min-w-9 items-center justify-end'>
             <span className='sr-only'>Search</span>
             <Suspense
@@ -107,11 +157,11 @@ const Navbar = ({
           </label>
         </div>
 
-        <CartSummaryNav />
-        {/* Center: Search */}
-
-        {/* Right: Icons */}
         <div className='flex items-center space-x-4'>
+          <CartSummaryNav />
+          {/* Center: Search */}
+
+          {/* Right: Icons */}
           {/* <Link
             href='/wishlist'
             className='relative rounded p-2 hover:bg-gray-100'>
@@ -127,7 +177,11 @@ const Navbar = ({
           </Button> */}
 
           {/* <CommandBar /> */}
-          <ProfileDropdown user={user} navLinks={navLinks} />
+          <ProfileDropdown
+            categoriesData={categoriesData}
+            user={user}
+            navLinks={navLinks}
+          />
         </div>
       </div>
     </header>
